b05e6c33568eef6021350ff6dde645862c84957d
Mohan Poluri <polurimohan1@gmail.com>
2025-12-25 21:57:41 +0530

    first upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let key;
+let min;
+let max;
+function genNew(){
+     min=document.getElementById("min-val").value;
+     max=document.getElementById("max-val").value;
+    if(min==""||max==""){
+        alert("Enter both min and max value");
+        return;
+    }
+    min=parseInt(min);
+    max=parseInt(max);
+    if(max<=min){
+        alert("Maximum should be grater than Minimum");   
+        return; 
+    }
+    key=Math.floor(Math.random()*(max-min+1))+min;
+    document.querySelector(".status").textContent="Value generated! Start guessing 🎯";
+}
+function checkVal(){
+    let guess=document.getElementById("guessed-val").value;
+    let msg=document.querySelector(".msg").textContent ;
+    if(guess==""){
+        alert("Guess the number first !!");
+    }
+    guess=parseInt(guess);
+    if(guess>max||guess<min){
+        alert("Value is not with in the range");
+        return;
+    }
+    if(guess===key){
+        document.querySelector(".status").textContent="You won the game 🥳";
+        return;
+    }else if(guess>key){
+        document.querySelector(".status").textContent="Your guess is greater than the value ⬆️";
+    }else{
+        document.querySelector(".status").textContent="Your guess is smaller than the value ⬇️";
+    }
+}
